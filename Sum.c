d03a9383353b784a00815920a5dbb8bdee8004a2
Rachana <rtamane@my.okcu.edu>
2019-03-27 10:17:48 -0500

    This is my second version v

--- Sum.c
+++ Sum.c
@@ -1,17 +1,24 @@
 //Author: Rachana Tamane
-//Description: Sum of 1 to n
+//Description: Sum of 1 to n and their average
 
 #include<stdio.h>
 main(){
-  int i,num, sum=0; //initilazing
+  int i,num,count, sum=0; //initilazing
+  float average;
+
   
   printf("Please enter a number: \n"); //entering the value of number
   scanf("%d", &num);
   printf("Enter a value: \n",&num);
-  for(i=1;i<=num;i++)
-    sum +=i;
+
   
+  for(i=1;i<=num;i++){
+    sum +=i;
+   count =1;
+  }
 
     printf("Sum of numbers from 1 to %d is %d \n",num, sum);
+    printf("The average of the number is: %f \n", sum/num);
 
+	   
  }//end of main
